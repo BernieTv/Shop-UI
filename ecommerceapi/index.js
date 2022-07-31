d946009960a150fd859bebb82a700b5431d3a16f
Bekzod Tukhtasinov <86732898+BernieTv@users.noreply.github.com>
2022-07-31 16:15:58 +0500

    creating rest api and connecting to mongoose

---
+++ ecommerceapi/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+
+dotenv.config();
+
+const app = express();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('DB Success'))
+	.catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('/api/users', userRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log('Backend server is running!');
+});
